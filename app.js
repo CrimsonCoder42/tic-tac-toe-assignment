7db3ac56ced655b382d633126b89c64ebabce417
Nostro37 <nostro37@gmail.com>
2022-02-08 14:55:11 -0500

    background and button removal

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 
-
+alert('hello')
 
 //create grid in JavaScript 
-for (i = 0; i < 3; i++){
+for (i = 0; i < 9; i++){
     var div = document.createElement("div");
     div.className = "grid";
+    div.id = `grid ${i}`
     document.getElementById("main").appendChild(div);
-    for (j = 0; j < 2; j++){
-        var div = document.createElement("div");
-        div.className = "grid";
-        document.getElementById("main").appendChild(div);
-    }
+    
 }
