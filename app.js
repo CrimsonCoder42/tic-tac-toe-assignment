8565fc226cb668dadca19b52881f805bcc41e553
Nostro37 <nostro37@gmail.com>
2022-02-09 09:01:56 -0500

    logic anf HTML updatye

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 for (i = 0; i < 9; i++){
     var div = document.createElement("div");
     div.className = "grid";
-    div.id = `grid ${i}`
+    div.id = `grid${i}`
     document.getElementById("main").appendChild(div);
     
 }
