fbc2ccb07d4083f504a593494c297eac417596c9
Nostro37 <nostro37@gmail.com>
2022-02-13 17:51:25 -0500

    updaes to grid and background done

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
+// X = player1 since X goes first. 
+let whoTurn;
+const player1 = "EX";
+const player2 = "O";
 
-//create grid in JavaScript 
-for (i = 0; i < 9; i++){
-    var div = document.createElement("div");
-    div.className = "grid";
-    div.id = `grid${i}`
-    document.getElementById("main").appendChild(div);
-    
+// All possible win options
+const toWin = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+createGrid()
+
+//grab all elements needed and assign to variables
+const alertMessage = document.getElementById('messageAlert');
+const grids = document.querySelectorAll('.grid')
+const playButton = document.getElementById('play');
+
+// Create grid generate gird ID numbers 
+function createGrid() {
+    for (i = 0; i < 9; i++){
+        var div = document.createElement("div");
+        div.className = "grid";
+        div.id = `grid${i}`
+        document.getElementById("main").appendChild(div);
+    }
 }
+
+playGame()
+
+
+function playGame() {
+    whoTurn = false;
+
+}
+
+
