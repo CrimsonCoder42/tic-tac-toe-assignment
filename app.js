854b421f09b880c5ffeaebd16268dc7773118db5
Nostro37 <nostro37@gmail.com>
2022-02-14 09:32:53 -0500

    X and O logic created

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // X = player1 since X goes first. 
 let whoTurn;
-const player1 = "EX";
-const player2 = "O";
+const player1 = "grid EX";
+let player1Array = [];
+const player2 = "grid O";
+let player2Array = [];
 
 // All possible win options
 const toWin = [
@@ -16,27 +18,56 @@ const toWin = [
 ]
 createGrid()
 
-//grab all elements needed and assign to variables
+//grab all elements needed and assign to variables, add event listener to button
 const alertMessage = document.getElementById('messageAlert');
 const grids = document.querySelectorAll('.grid')
 const playButton = document.getElementById('play');
+playButton.addEventListener('click', playGame);
 
 // Create grid generate gird ID numbers 
 function createGrid() {
     for (i = 0; i < 9; i++){
         var div = document.createElement("div");
         div.className = "grid";
-        div.id = `grid${i}`
+        div.id = `${i}`
         document.getElementById("main").appendChild(div);
     }
 }
 
 playGame()
-
+grids.forEach(grid => {
+})
 
 function playGame() {
     whoTurn = false;
+    grids.forEach(grid => {
+        grid.classList = "grid"
+        grid.innerText = ""
+        player1Array = [];
+        player2Array = [];
+        grid.removeEventListener('click', myClick)
+        grid.addEventListener('click', myClick, { once: true })
 
+    })
+}
+function myClick(event){
+    clickId = event.target.id
+    console.log(whoTurn)
+    if (!whoTurn) {
+        event.target.className = player1
+        player1Array.push(parseInt(clickId))
+        console.log(player1Array)
+        whoTurn = true;
+    } else {
+        event.target.className = player2
+        player2Array.push(parseInt(clickId))
+        event.target.innerText = "O"
+        console.log(player2Array)
+        whoTurn = false;
+    }
+    
 }
 
-
+function play() {
+    
+}
